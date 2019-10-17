628596ba6eb57cdd4cb092d79090e212d932abf3
sige <837337164@qq.com>
2019-10-17 21:29:39 +0800

    2019-10-17

---
+++ c_code/day03/01_hello.c
@@ -0,0 +1,24 @@
+/*********************************************************************************
+*    Filename: 01_hello.c
+* Description:
+*     Version: 1.0
+*     Created: 2019年10月17日  20时47分01秒
+*    Revision: none 
+*    Compiler: gcc
+*      Author: YOUR NAME (),
+*     Company:
+* gcc 01_hello.c
+* gcc 01_hello.c -o  hello
+* ./a.out
+* ./hello 
+**********************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   //这是第一个C代码
+   printf("hello mike\n");
+
+   return 0;
+}
