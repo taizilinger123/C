cd1f13f124181236c873c4d2cef48cce740ad379
taizilinger123 <837337164@qq.com>
2022-01-15 23:37:39 +0800

    20220115

--- day02/03var.c
+++ day02/03var.c
@@ -5,5 +5,10 @@
 int main(){
   int  num = 3, num1 = 7;
   printf("num是%d,num1是%d\n", num, num1);
+  int num2 = 0;
+  num2 = num;
+  num = num1;
+  num1 = num2;
+  printf("num是%d,num1是%d\n", num, num1);
   return 0;
 }
