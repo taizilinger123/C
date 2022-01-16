d1b2af5909d73d78f2a106a7e8b7a7e0f1f7ddd2
taizilinger123 <837337164@qq.com>
2022-01-16 15:46:11 +0800

    20220116

---
+++ day03/03var.c
@@ -0,0 +1,17 @@
+/*
+ 变量练习
+ */
+#include <stdio.h>
+int main(){
+   int num = 1;
+   printf("%d X %d = %d\n", num, 10 - num, num * (10 - num));
+   num = 2;
+   printf("%d X %d = %d\n", num, 10 - num, num * (10 - num));
+   num = 3;
+   printf("%d X %d = %d\n", num, 10 - num, num * (10 - num));
+   num = 4;
+   printf("%d X %d = %d\n", num, 10 - num, num * (10 - num));
+   num = 5;
+   printf("%d X %d = %d\n", num, 10 - num, num * (10 - num));
+   return 0;
+}
