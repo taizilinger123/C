bc8f0fc216dc10c033c40fc18928d570cd052bc4
taizilinger123 <837337164@qq.com>
2022-01-16 19:29:38 +0800

    20220116

--- day04/08scanf.c
+++ day04/08scanf.c
@@ -3,9 +3,9 @@
  */
 #include <stdio.h>
 int main(){
-   int num = 0;
-   printf("请输入一个数字：");
-   scanf("%d", &num);
-   printf("%d\n", num);
+   int num = 0, num1 = 0;
+   printf("请输入两个数字：");
+   scanf("%d %d", &num, &num1);
+   printf("%d %d\n", num, num1);
    return 0;
 }
