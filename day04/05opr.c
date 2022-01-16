87d600117556d52718f36f2cfa31c41b4f3c43b8
taizilinger123 <837337164@qq.com>
2022-01-16 16:57:30 +0800

    20220116

---
+++ day04/05opr.c
@@ -0,0 +1,9 @@
+/*
+   操作符练习
+   */
+#include <stdio.h>
+int main(){
+   printf("15 / 6是%d\n", 15 / 6);
+   printf("15 %% 6是%d\n", 15 % 6);
+   return 0;
+}
---
+++ day04/06tri.c
@@ -0,0 +1,18 @@
+/*
+   变量练习
+   */
+#include <stdio.h>
+int main(){
+   int num = 54321;
+   printf("%d\n", num);
+   num = num / 10;
+   printf("%d\n", num);
+   num = num / 10;
+   printf("%d\n", num);
+   num = num / 10;
+   printf("%d\n", num);
+   num = num / 10;
+   printf("%d\n", num); 
+   num = num / 10;
+   return 0;
+}
