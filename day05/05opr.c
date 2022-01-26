e68fd0398bc5a3d0d56b753758689cc283373241
taizilinger123 <837337164@qq.com>
2022-01-26 19:44:32 +0800

    20220126

--- day05/05opr.c
+++ day05/05opr.c
@@ -2,6 +2,7 @@
    自增，自减操作符练习
  */
 #include <stdio.h>
+int num2 = 5;
 int main(){
   int num = 0, num1 = 0;
   num++;
@@ -15,5 +16,12 @@ int main(){
   printf("num是%d\n", num);
   num1 = num++;
   printf("num是%d,num1是%d\n", num, num1);
+  num1 = ++num;
+  printf("num是%d,num1是%d\n", num, num1);
+  num = 5;
+  num1 = num++ + ++num;
+  printf("num是%d,num1是%d\n", num, num1);
+  num1 = num2++ + ++num2;
+  printf("num1是%d,num2是%d\n", num1, num2);
   return 0;
 }
