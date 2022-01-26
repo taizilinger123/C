0431fcfc22d5e97f0a7da04e501b8120feffe373
taizilinger123 <837337164@qq.com>
2022-01-26 22:02:27 +0800

    20220126

--- day05/06bool.c
+++ day05/06bool.c
@@ -5,8 +5,19 @@
 #include <stdbool.h>
 int main(){
   bool bl = true;
+  int num = 0;
   printf("bl是%d\n", bl);
   bl = false;
   printf("bl是%d\n", bl);
+  printf("3 == 5是%d\n", 3 == 5);
+  printf("3 != 5是%d\n", 3 != 5);
+  printf("!6是%d\n", !6);
+  num = 3 != 2 + 1;
+  printf("num是%d\n", num);
+  num = 7;
+  0 && ++num;
+  printf("num是%d\n", num);
+  1 || ++num;
+  printf("num是%d\n", num);
   return 0;
 }
---
+++ day05/07bit.c
@@ -0,0 +1,10 @@
+/*
+   位操作符练习
+ */
+#include <stdio.h>
+int main(){
+   printf("3 & 5是%d\n", 3 & 5);
+   printf("3 | 5是%d\n", 3 | 5);
+   printf("3 ^ 5是%d\n", 3 ^ 5);
+   return 0;
+}
