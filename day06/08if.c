4a354a0b78145ab367d67ed29e6e8776128411aa
taizilinger123 <837337164@qq.com>
2022-02-08 22:29:41 +0800

    20220208

---
+++ day06/08if.c
@@ -0,0 +1,19 @@
+/*
+ 分支练习
+ */
+#include <stdio.h>
+int main(){
+   int num = 0;
+   printf("请输入一个整数：");
+   scanf("%d", &num);
+   if (num < 0){
+       printf("负数\n");
+   }
+   else if (num > 0){
+       printf("正数\n");
+   }
+   else  {
+       printf("零\n");
+   }
+   return 0;
+}
