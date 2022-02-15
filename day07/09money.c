b3e2d9e010c4454fdd05eb10c2fa62c86bf8c4cd
taizilinger123 <837337164@qq.com>
2022-02-15 21:29:32 +0800

    20220215

--- day07/09money.c
+++ day07/09money.c
@@ -6,7 +6,9 @@ int main(){
   int num1 = 0, num2 = 0;
   for(num1 = 0;num1 <= 2;num1++){
       for(num2 = 0;num2 <= (10 - 5 * num1)/2;num2++){
-         printf("5元钱一共有%d张，2元钱一共有%d张，1元钱一共有%d张\n", num1, num2,10 - 5*num1 - 2*num2);
+         printf("5元钱一共有%d张，2元钱一共" 
+				 "有%d张，1元钱一共有%d张\n",  \ 
+				 num1, num2,10 - 5*num1 - 2*num2);
       }
   }
   return 0;
