3f7c6e475159d30d565fe9d2aa9d53063ed9a057
taizilinger123 <837337164@qq.com>
2022-02-27 17:23:53 +0800

    20220227

--- day09/07func.c
+++ day09/07func.c
@@ -5,11 +5,23 @@
 int add(int num, int num1){
    return num + num1;
 }
+void print(int num){
+   printf("%d\n", num);
+}
+int read(void){
+   int num = 0;
+   printf("请输入一个数字：");
+   scanf("%d", &num);
+   return num;
+}
 int main(){
    int num = 0;
    num = add(2, 3);
    printf("num是%d\n", num);
+   print(num);
+   num = read();
    num = add(num, 4);
    printf("num是%d\n", num);
+   print(num);
    return 0;
 }
