e25f4f8a0c0437202e727bb37acad6209620553a
taizilinger123 <837337164@qq.com>
2022-03-13 22:40:43 +0800

    20220313

--- day11/10ptr.c
+++ day11/10ptr.c
@@ -10,7 +10,7 @@ int main(){
    p_num2 = &num2;
    printf("请输入三个数字：");
    scanf("%d %d %d", p_num, p_num1, p_num2);
-   if (*p_num < *p_num1){
+   /*if (*p_num < *p_num1){
        *p_num += *p_num1;
 	   *p_num1 = *p_num - *p_num1;
 	   *p_num = *p_num - *p_num1;
@@ -19,6 +19,29 @@ int main(){
        *p_num += *p_num2;
 	   *p_num2 = *p_num - *p_num2;
 	   *p_num = *p_num - *p_num2;
+   }*/
+   //还需要在进行类型转换
+   /*if (*p_num < *p_num1){
+       if (*p_num < *p_num1){
+	       p_num = (int)p_num - (int)p_num1;
+		   p_num1 = (int)p_num + (int)p_num1;
+		   p_num = (int)p_num1 - (int)p_num;
+	   }
+	   if (*p_num < *p_num2){
+	       p_num = (int)p_num - (int)p_num2;
+		   p_num2 = (int)p_num + (int)p_num2;
+		   p_num = (int)p_num2 - (int)p_num;
+	   }
+   }*/
+   if (*p_num < *p_num1){
+      int *p_tmp = p_num;
+      p_num = p_num1;
+	  p_num1 = p_tmp;
+   }
+   if (*p_num < *p_num2){
+      int *p_tmp = p_num;
+	  p_num = p_num2;
+	  p_num2 = p_tmp;
    }
    printf("最大数是%d\n", *p_num);
    return 0;
