7328ece935231e55fd1ee791c6804caad4c1fb38
taizilinger123 <837337164@qq.com>
2022-03-31 00:05:43 +0800

    20220331

---
+++ day13/01strlen.c
@@ -0,0 +1,20 @@
+/*
+ mystrlen练习
+ */
+#include <stdio.h>
+//const修饰的是哪个形参指针的值不需要改变
+int mystrlen(const char *p_str){
+    //第二个变量是初始变量, \0 - 第一个字符串 = 字符串的长度
+    const char *p_str1 = p_str;
+	//p_str1不为\0的时候
+	while (*p_str1) {
+	   //p_str1向后移位
+	   p_str1++;
+	}
+	//p_str1 - p_str就是最后一个字符串\0减去第一个字符串算出字符串的个数
+	return p_str1 - p_str;
+}
+int main(){
+   printf("长度是%d\n", mystrlen("abcde"));
+   return 0;
+}
