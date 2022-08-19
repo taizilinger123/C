e8c1e5bf1771293631054163edda240793255e7a
taizilinger123 <837337164@qq.com>
2022-08-19 13:23:08 +0800

    20220819

---
+++ C++/day01/first.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main()
+{
+   std::cout<<"hello world"<<std::endl;
+   std::cout<<"hello c++"<<std::endl;
+}
+/*
+ root@test:/home/test/biaoc/C++/day01# gcc first.cpp  -lstdc++
+ root@test:/home/test/biaoc/C++/day01# ls
+ a.out  first.cpp
+ root@test:/home/test/biaoc/C++/day01# ./a.out
+ hello world
+ hello c++
+########################################
+root@test:/home/test/biaoc/C++/day01# g++ first.cpp 
+root@test:/home/test/biaoc/C++/day01# ls
+a.out  first.cpp
+root@test:/home/test/biaoc/C++/day01# ./a.out
+hello world
+hello c++
+ */
