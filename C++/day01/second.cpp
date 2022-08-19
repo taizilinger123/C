393323eeb18cb6c625e27ab51fcb61c3223d69d7
taizilinger123 <837337164@qq.com>
2022-08-19 13:55:42 +0800

    20220819

---
+++ C++/day01/second.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+   cout<<"hello world"<<endl;
+   cout<<"hello c++"<<endl;
+}
+/*
+ root@test:/home/test/biaoc/C++/day01# gcc first.cpp  -lstdc++
+ root@test:/home/test/biaoc/C++/day01# ls
+ a.out  first.cpp
+ root@test:/home/test/biaoc/C++/day01# ./a.out
+ hello world
+ hello c++
+########################################
+root@test:/home/test/biaoc/C++/day01# g++ first.cpp 
+root@test:/home/test/biaoc/C++/day01# ls
+a.out  first.cpp
+root@test:/home/test/biaoc/C++/day01# ./a.out
+hello world
+hello c++
+ */
---
+++ C++/day01/thrid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"hello world"<<endl;
+   cout<<"hello c++"<<std::endl;
+}
+/*
+ root@test:/home/test/biaoc/C++/day01# gcc first.cpp  -lstdc++
+ root@test:/home/test/biaoc/C++/day01# ls
+ a.out  first.cpp
+ root@test:/home/test/biaoc/C++/day01# ./a.out
+ hello world
+ hello c++
+########################################
+root@test:/home/test/biaoc/C++/day01# g++ first.cpp 
+root@test:/home/test/biaoc/C++/day01# ls
+a.out  first.cpp
+root@test:/home/test/biaoc/C++/day01# ./a.out
+hello world
+hello c++
+ */
