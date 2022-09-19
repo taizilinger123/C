60fb8b444b550470e05391645caa81bd3483013d
taizilinger123 <837337164@qq.com>
2022-09-20 00:59:56 +0800

    20220920

--- C++/day03/structobj.cpp
+++ C++/day03/structobj.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 struct Date{
    int year;
@@ -19,13 +20,13 @@ struct Date{
 int main(){
    cout<<sizeof(Date)<<endl;
    /*如何创建对象 */
-   Date date; //在栈里创建对象
+   Date date; //在栈里创建对象,生命期短
    //date.year=2013;
    date.setDate();
    date.showDate();
    date.setDate(2013,11,11);
    date.showDate();
-   /*在堆中创建对象*/
+   /*在堆中创建对象,生命期长*/
    Date *date2=new Date;
    date2->setDate();
    date2->showDate();
@@ -33,4 +34,8 @@ int main(){
    date2=NULL;
    /*使用malloc申请一个Date类型的对象空间
 	 给这个空间设置值 显示这个对象的数据*/
+   Date* date3=static_cast<Date*>(malloc(sizeof(Date)));
+   //malloc返回的是void*
+   date3->setDate(2008,8,8);
+   date3->showDate();
 }
