d2e7dfb9db3ec2ef91d6fcfe8797b059099fa3a5
taizilinger123 <837337164@qq.com>
2022-09-26 00:21:38 +0800

    20220926

--- C++/day04/const.cpp
+++ C++/day04/const.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 using namespace std;
 class A{
+  //如果你想修改对象的成员变量,则成员变量需要修饰成mutable
+  mutable int data;
   public:
   //注意一下const参数加的位置
   void show()const{
+	//const 函数 不能修改对象的成员变量,只能读cout
+	data=1;
+	//cout<<data<<endl;
+	//const函数  只能调用const函数
+	//readData();
     cout<<"this is const show()"<<endl;
   }
-  void show(){
-    cout<<"this is show()"<<endl;
+  void readData(){
+     cout<<data<<endl;
   }
+  /*void show(){
+    cout<<"this is show()"<<endl;
+  }*/
 };
 int main(){
    A  a;
+   //非const对象优先选择  非const函数  如果没有非const  函数则调用const函数
    a.show();
    //const对象必须初始化
    const A b=a;
    b.show();
 }
+
+/*
+   root@test:/home/test/biaoc/C/C++/day04# g++ -S  const.cpp生成汇编
+   root@test:/home/test/biaoc/C/C++/day04# vi const.s
+   11 _ZNK1A4showEv:  有const参数的函数多了个K
+   41 _ZN1A4showEv:   没有const参数的函数
+*/
---
+++ C++/day04/const.s
@@ -0,0 +1,150 @@
+	.file	"const.cpp"
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.section	.rodata
+.LC0:
+	.string	"this is const show()"
+	.section	.text._ZNK1A4showEv,"axG",@progbits,_ZNK1A4showEv,comdat
+	.align 2
+	.weak	_ZNK1A4showEv
+	.type	_ZNK1A4showEv, @function
+_ZNK1A4showEv:
+.LFB1021:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movl	$.LC0, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, %esi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1021:
+	.size	_ZNK1A4showEv, .-_ZNK1A4showEv
+	.section	.rodata
+.LC1:
+	.string	"this is show()"
+	.section	.text._ZN1A4showEv,"axG",@progbits,_ZN1A4showEv,comdat
+	.align 2
+	.weak	_ZN1A4showEv
+	.type	_ZN1A4showEv, @function
+_ZN1A4showEv:
+.LFB1022:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movl	$.LC1, %esi
+	movl	$_ZSt4cout, %edi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, %esi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1022:
+	.size	_ZN1A4showEv, .-_ZN1A4showEv
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1023:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-10(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZN1A4showEv
+	leaq	-9(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNK1A4showEv
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1023:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1032:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L8
+	cmpl	$65535, -8(%rbp)
+	jne	.L8
+	movl	$_ZStL8__ioinit, %edi
+	call	_ZNSt8ios_base4InitC1Ev
+	movl	$__dso_handle, %edx
+	movl	$_ZStL8__ioinit, %esi
+	movl	$_ZNSt8ios_base4InitD1Ev, %edi
+	call	__cxa_atexit
+.L8:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1032:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB1033:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1033:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
