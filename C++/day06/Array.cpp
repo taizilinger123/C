b8ca9c4f38d0d4d315b286ec19de7ee839d6dc0c
taizilinger123 <837337164@qq.com>
2022-10-27 23:54:21 +0800

    20221027

--- C++/day06/Array.cpp
+++ C++/day06/Array.cpp
@@ -42,11 +42,37 @@ class Array{
 		return *this;
 	}
 	/* [] 运算符 int operator[](int ind) */
-
+	int& operator[](int ind){
+	    if(ind>=size){
+		   throw "下标越界";
+		}
+		return data[ind];
+	}
+    /*提供一个函数 放入数据*/
+	Array& push_back(int d){
+	    if(size+1<=maxsize){
+		   data[size++]=d;
+		}else{
+		   throw "数组已满";
+		}
+		return *this;
+	}
 };
 int main(){
-    Array  a(1);
+    Array  a(5);
+	a.push_back(100);
+	a.push_back(300).push_back(200).push_back(2).push_back(3);
+	/*
+	 terminate called after throwing an instance of 'char const*'
+	 已放弃 (核心已转储)
+	 */
+    try{
+	   a.push_back(555);
+	}catch(const char* e){
+	   cout<<e<<endl;
+	}
+	cout<<a[1]<<endl;
 	Array  b(2);
 	//b=a;
-	a=a;
+	//a=a;
 }
