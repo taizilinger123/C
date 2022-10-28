72a188d27f4e826eb2a41b9184b31d006768df06
taizilinger123 <837337164@qq.com>
2022-10-28 22:35:17 +0800

    20221028

---
+++ C++/day06/autoptr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class A{
+   public:
+   A(){
+      cout<<"A()"<<endl;
+   }
+   ~A(){
+      cout<<"~A()"<<endl;
+   }
+   void show(){
+      cout<<"show()"<<endl;
+   }
+};
+class autoptr{
+   A*  data;
+   public:
+   autoptr(A* data):data(data){
+      
+   }
+   ~autoptr(){
+     delete data;
+   }
+   /*重载->运算符*/
+   A* operator->(){
+     return data;
+   }
+};
+int main(){
+   A*  a=new A();
+   autoptr atr(a);
+   atr->show();
+   atr.operator->()->show();
+}
