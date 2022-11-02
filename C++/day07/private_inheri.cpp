3975ac5e4459b38d02eedef59ce100f02271654a
taizilinger123 <837337164@qq.com>
2022-11-03 01:37:35 +0800

    20221103

---
+++ C++/day07/private_inheri.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class A{
+   private:int x;
+   protected:int y;
+   public:int z;
+   public:A(int x=100,int y=200,int z=400){
+     this->x=x;this->y=y;this->z=z;		  
+   }
+   /*上面写法跟下面一样*/
+   //public:A(int x=100,int y=200,int z=400):x(x),y(y),z(z){}
+   public:
+   void showx(){
+      cout<<"x="<<x<<endl;
+   }
+};
+class B:private A{/*y z权限会变成私有的*/
+   public:
+   void showyz(){
+	  showx();
+      cout<<"y="<<y<<" "<<"z="<<z<<endl;
+   }
+   /*如何访问到A类的私有成员*/
+   
+};
+//class C:public B{/*y z权限会变成私有的*/};
+int main(){
+  B  b;
+  b.showyz();
+}
