5cf06ae4fefaa00d91aec74b8aac70343895329b
taizilinger123 <837337164@qq.com>
2022-11-07 01:45:11 +0800

    20221107

--- C++/day07/private_inheri.cpp
+++ C++/day07/private_inheri.cpp
@@ -21,10 +21,16 @@ class B:private A{/*y z权限会变成私有的*/
       cout<<"y="<<y<<" "<<"z="<<z<<endl;
    }
    /*如何访问到A类的私有成员*/
-   
+
+   friend void fshowyz(B b);  
 };
+void fshowyz(B b){
+   cout<<b.y<<":"<<b.z<<endl;
+}
 //class C:public B{/*y z权限会变成私有的*/};
 int main(){
   B  b;
+  fshowyz(b);
+  cout<<"---------------"<<endl;
   b.showyz();
 }
