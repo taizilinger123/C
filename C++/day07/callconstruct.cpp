ff655d58cb02e49d5b0ee363fcb46e6ad2b132c3
taizilinger123 <837337164@qq.com>
2022-11-07 02:12:43 +0800

    20221107

---
+++ C++/day07/callconstruct.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class A{
+   public:
+   A(){
+      cout<<"A()"<<endl;
+   }
+   ~A(){
+      cout<<"~A()"<<endl;
+   }
+};
+class B:public A{
+    public:
+    B(){
+	   cout<<"B()"<<endl;
+	}
+	~B(){
+	   cout<<"~B()"<<endl;
+	}
+};
+class C:public B{
+    public:
+	C(){
+	   cout<<"C()"<<endl;  
+	}
+	~C(){
+	   cout<<"~C()"<<endl;
+	}
+};
+int main(){
+   //B  b;
+     C  c;
+}
