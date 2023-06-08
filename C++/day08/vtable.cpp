8b18ca5fcc46419c643c99eed7af3b165d74a171
taizilinger123 <837337164@qq.com>
2023-06-08 13:46:14 +0800

    20230608

--- C++/day08/vtable.cpp
+++ C++/day08/vtable.cpp
@@ -23,9 +23,12 @@ typedef void (*VFUN) (A* a,int);
 typedef VFUN* VTABLE;
 //这个意思是定义VFUN*为VTABLE
 //VFUN*的意思是*VFUN指针的指针
+//VTABLE是VFUN的数组，这个数组装了一个一个VFUN的元素, VFUN*定义出VTABLE来, int*表达int的数组
 int main(){
     A a(99);
     VTABLE vt=*((VTABLE*)&a);
+	//VTABLE vt这个是表示VTABLE的对象是vt,怎么取得A a(99)这个对象的虚函数表呢？
+	//&a的意思是取了这个对象a的前4个字节，把这个前4个字节的值看作VTABLE*,把VTABLE*的值拿出来就要前面加*
 	/*直接使用虚函数表中的地址进行调用*/
 	vt[0](&a,10001);
 	vt[1](&a,10);
