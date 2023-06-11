bb48d5dd9f3a0367bbed53f7b6269c2604bdd17b
taizilinger123 <837337164@qq.com>
2023-06-11 20:09:20 +0800

    20230611

--- C++/day08/typeid.cpp
+++ C++/day08/typeid.cpp
@@ -3,11 +3,37 @@
 using namespace std;
 class Animal{
      public:
-     virtual void show(){}
+     virtual void show(){
+	    cout<<"这是动物"<<endl;
+	 }
 };
 class Dog:public Animal{
-
+     public:
+     void show(){
+	    cout<<"这是狗"<<endl;
+	 }
+	 void fun(){
+	    cout<<"可以看家"<<endl;
+	 }
 };
+class Cat:public Animal{
+	public:
+    void show(){
+	    cout<<"这是猫"<<endl;
+	}
+	void catchMouse(){
+	    cout<<"抓老鼠"<<endl;
+	}
+};
+void  testfun(Animal* a){
+   /*在这里识别传入的对象是什么具体类型*/
+   a->show();
+   if(typeid(*a)==typeid(Cat)){
+       ((Cat*)a)->catchMouse();
+   }else if(typeid(*a)==typeid(Dog)){
+       ((Dog*)a)->fun();
+   }
+}
 int main(){
    int a;
    cout<<typeid(a).name()<<endl;
@@ -28,4 +54,9 @@ int main(){
    cout<<typeid(pa).name()<<endl;
    /*必须满足多态性 */
    cout<<typeid(*pa).name()<<endl;
+   cout<<"----------------"<<endl;
+   Dog tdog;
+   Cat tcat;
+   testfun(&tdog);
+   testfun(&tcat);
 }
