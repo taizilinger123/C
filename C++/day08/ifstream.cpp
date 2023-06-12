5ea1e600e6cd1800315d0da5c4eab8234d8cb1ea
taizilinger123 <837337164@qq.com>
2023-06-12 16:13:51 +0800

    20230612

--- C++/day08/ifstream.cpp
+++ C++/day08/ifstream.cpp
@@ -7,8 +7,14 @@ int main(){
       cout<<"open file failed"<<endl;
    }
    char c;
-   while((c=ifs.get())!=EOF){
+   /*while((c=ifs.get())!=EOF){
       cout.put(c);
+   }*/
+   while(ifs){
+	  /*格式化输入*/
+      ifs>>c;
+	  cout.put(c);
    }
    ifs.close();
+   /*使用ifs.get(c)读取文件 然后完成复制文件*/
 }
