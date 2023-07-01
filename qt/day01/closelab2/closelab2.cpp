653a41b9bbe15caba7c6b344800b23c6b9a2f25b
taizilinger123 <837337164@qq.com>
2023-07-01 17:10:26 +0800

    20230701

---
+++ qt/day01/closelab2/closelab2.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+int main(int argc,char** argv){
+   QApplication app(argc,argv);
+   /*造一个父窗口对象*/
+   QWidget  *parent=new QWidget();
+   parent->resize(500,400);
+   parent->show();
+   QPushButton *qpush=new QPushButton("close",parent);
+   qpush->resize(100,100);
+   qpush->show();
+   return app.exec();
+}
