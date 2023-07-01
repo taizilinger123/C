165286c0847335d632812742419b7978b51fadbf
taizilinger123 <837337164@qq.com>
2023-07-01 17:19:56 +0800

    20230701

--- qt/day01/closelab2/closelab2.cpp
+++ qt/day01/closelab2/closelab2.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QLabel>
 int main(int argc,char** argv){
    QApplication app(argc,argv);
    /*造一个父窗口对象*/
@@ -11,5 +12,9 @@ int main(int argc,char** argv){
    qpush->resize(100,100);
    qpush->move(200,250);
    qpush->show();
+   /*创建标签对象 设置父窗口指针 移动位置*/
+   QLabel  *qlab=new QLabel("hello qt",parent);
+   qlab->move(200,50);
+   qlab->show();
    return app.exec();
 }
