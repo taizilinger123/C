dd3db3562a565a95e38a9fe9c476ce3870c80997
taizilinger123 <837337164@qq.com>
2023-07-05 16:56:40 +0800

    20230705

--- qt/day02/adder2/adder.cpp
+++ qt/day02/adder2/adder.cpp
@@ -5,6 +5,7 @@
 	   QLineEdit   *res;
 	   QPushButton *bclose;*/
 #include  "adder.h"
+#include  <QString>
 Adder::Adder(){
     this->resize(660,300);
 	/*给组件赋值*/
@@ -36,11 +37,15 @@ Adder::~Adder(){
 }
 /*自定义槽函数 实现计算*/
 void  Adder::getRes(){
-    res->setText("9527");
+    //res->setText("9527");
 	/*1.得到add和added的输入值
 	  2.把得到的QString的值变成int
-	  3.相加得到和 
-	  4.把和变成QString类型最后赋值给res
-	 */
-
+	  3.相加得到和 把和变成QString类型
+	  4.最后赋值给res*/
+	QString  sadd=add->text();
+	QString  sadded=added->text();
+    int  iadd=sadd.toInt();
+	int  iadded=sadded.toInt();
+	int  sum=iadd+iadded;
+	res->setText(QString::number(sum));
 }
