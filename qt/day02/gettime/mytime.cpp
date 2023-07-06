2f4792a8ff945953c23d462d83af6979a0408513
taizilinger123 <837337164@qq.com>
2023-07-06 14:21:26 +0800

    20230706

---
+++ qt/day02/gettime/mytime.cpp
@@ -0,0 +1,27 @@
+#include "mytime.h"
+#include <QApplication>
+MyTime::MyTime(){
+    res=new QLabel("current time",this);
+	gettime=new QPushButton("gettime",this);
+	this->resize(400,500);
+	/*移动组件的位置*/
+	res->move(200,50);
+	gettime->move(200,400);
+	/*绑定按钮 到槽函数*/
+	connect(gettime,SIGNAL(clicked()),this,SLOT(setTime()));
+}
+MyTime::~MyTime(){
+    delete res;
+	delete gettime;
+}
+#include <QTime>
+void MyTime::setTime(){
+    /*调用setText*/
+	res->setText(QTime::currentTime().toString("hh:mm:ss"));
+}
+int main(int argc,char** argv){
+    QApplication  app(argc,argv);
+	MyTime mt;
+	mt.show();
+	return  app.exec();
+}
---
+++ qt/day02/gettime/mytime.h
@@ -0,0 +1,18 @@
+#ifndef MYTIME_H
+#define MYTIME_H
+#include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+   class  MyTime:public QDialog{
+	   Q_OBJECT
+       private:
+	   QLabel        *res;
+	   QPushButton   *gettime;
+	   public:
+	   MyTime();
+	   ~MyTime();
+	   /*提供一个槽函数  和按钮绑定 并调用标签的槽函数setText(QString)*/
+	   public slots:
+	   void  setTime();
+   };
+#endif
