48041e9dbb9f058d121de01e73da67282e6cd983
taizilinger123 <837337164@qq.com>
2023-08-20 21:39:23 +0800

    20230820

--- stl/day01/array.cpp
+++ stl/day01/array.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <typeinfo>
 using  namespace std;
-template<typename T, size_t S = 5>
+template<typename T = double, size_t S = 5>
+//template<typename T = double, size_t S> //错误,缺省参数必须靠右，前面带缺省参数，后面必须带缺省参数跟函数一样
 class Array{
 public:
   T&  operator[] (size_t i){
@@ -13,7 +15,7 @@ public:
       return S;
   }
 private:
-  T m_array[S];
+  T m_array[S];//int m_array[10];T=int, S=10
 };
 int main(void) {
    Array<int, 10> a1; // OK
@@ -29,6 +31,9 @@ int main(void) {
    cout << endl;
    Array<string> a2;
    cout<< a2.size() <<endl;
+   Array<> a3;//模板不传参数<>也必须写
+   cout<<a3.size()<<endl;
+   cout<<typeid(a3[0]).name()<<endl;
    return 0;
 }
 //模板是在编译时候传参数，不是在运行的时候传参
