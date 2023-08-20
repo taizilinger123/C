6fd90e23eafa02f382ecfa636fe04cfc38d956c4
taizilinger123 <837337164@qq.com>
2023-08-20 20:50:38 +0800

    20230820

---
+++ stl/day01/feeb.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <typeinfo>
+//typeinfo是打印T的类型
+using namespace std;
+template<typename T>
+class Feeb {
+public:
+   Feeb(void){
+     cout<< "Feeb<T>: "<<typeid(T).name()<<endl;
+   }
+};
+
+template<typename T>
+class Feeb<T[]>{
+//特化没有具体的数组的数字下面的也不能写具体的数组数字
+public:
+   Feeb (void) {
+     cout << "Feeb<T[]>: "<<typeid(T).name()<<endl; 
+   }
+};
+template<typename T>
+class Feeb<T*>{
+public:
+  Feeb(void){
+    cout<<"Feeb<T*>: "<<typeid(T).name()<<endl;
+  }
+};
+
+int main(void){
+   Feeb<char> f1;
+   Feeb<char[]> f2;//这里跟特化数组保持一致，不能写具体的数字
+   Feeb<char*> f3;
+   Feeb<char*****> f4;
+   return 0;
+}
+
