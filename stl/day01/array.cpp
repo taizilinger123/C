efdbaa51ced2e0d1b33c9fa547d857baa6b21a78
taizilinger123 <837337164@qq.com>
2023-08-20 21:46:30 +0800

    20230820

--- stl/day01/array.cpp
+++ stl/day01/array.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <typeinfo>
+#include <iomanip>
 using  namespace std;
 template<typename T = double, size_t S = 5>
 //template<typename T = double, size_t S> //错误,缺省参数必须靠右，前面带缺省参数，后面必须带缺省参数跟函数一样
@@ -34,6 +35,16 @@ int main(void) {
    Array<> a3;//模板不传参数<>也必须写
    cout<<a3.size()<<endl;
    cout<<typeid(a3[0]).name()<<endl;
+   cout<<setfill('$')<<setw(10)<<123<<endl;//一共10个字符宽度，空白的地方填$字符
    return 0;
 }
 //模板是在编译时候传参数，不是在运行的时候传参
+/*
+ root@test:/home/test/biaoc/C/stl/day01# g++  array.cpp 
+ root@test:/home/test/biaoc/C/stl/day01# ./a.out 
+ 0 1 2 3 4 5 6 7 8 9 
+ 5
+ 5
+ d
+ $$$$$$$123
+ */
