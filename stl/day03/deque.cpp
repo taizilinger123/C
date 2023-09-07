dad051335f9a0293aeeac6e25d01b7993255dd0b
taizilinger123 <837337164@qq.com>
2023-09-07 16:05:30 +0800

    20230907

--- stl/day03/deque.cpp
+++ stl/day03/deque.cpp
@@ -11,6 +11,28 @@ int main(void){
    di.push_front(30);
    print(di.begin(), di.end());
    //30 10 20 50 60 40
+   di.pop_back();
+   print(di.begin(), di.end());
+   //30 10 20 50 60
+   di.pop_front();
+   print(di.begin(), di.end());
+   //10 20 50 60
+   di.insert(di.begin() + 2, 70);
+   print(di.begin(), di.end());
+   //10 20 70 50 60
+   di.erase(di.end() - 1);
+   print(di.begin(), di.end());
+   //10 20 70 50
+   deque<int>::iterator it = find(di.begin(), di.end(), 70);
+   if (it == di.end())
+	   cout << "没找到!" << endl;
+   else
+	   cout << "找到了:" << *it << endl;
+   sort(di.begin(), di.end());
+   print(di.begin(), di.end());
+   for(size_t i = 0; i<di.size(); ++i)
+	   di[i] += 100;
+   print(di.begin(), di.end());
    return 0;
 }
 
