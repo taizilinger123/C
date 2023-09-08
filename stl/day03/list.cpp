176d93924a20c7d3c60a74a7c67c4b05ce7ca4c8
taizilinger123 <837337164@qq.com>
2023-09-08 11:06:06 +0800

    20230908

--- stl/day03/list.cpp
+++ stl/day03/list.cpp
@@ -1,5 +1,13 @@
 #include <list>
+#include <algorithm>
 #include "print.h"
+//比较器,比较a,b的大小a>b的话大的在前面倒序排列
+class Greater {
+public:
+  bool operator()(int a, int b) const {
+     return a > b;
+  }
+};
 int main(void) {
   list<int> li;
   li.push_back(50);
@@ -15,7 +23,47 @@ int main(void) {
   li.unique();
   print(li.begin(), li.end());
   list<int> ls;
-
+  ls.push_back(100);
+  ls.push_back(200);
+  ls.push_back(300);
+  ls.push_back(400);
+  ls.push_back(500);
+  list<int>::iterator pos = li.begin();
+  ++++++pos;
+  /*
+  li.splice(pos, ls);
+  list<int>::iterator del = ls.begin();
+  ++del;
+  li.splice(pos, ls, del);
+  */
+  list<int>::iterator begin = ls.begin();
+  begin++; // ->200
+  list<int>::iterator end = ls.end();
+  ----end; // ->400
+  li.splice(pos, ls, begin, end);
+  cout << "ls: ";
+  print(ls.begin(), ls.end());
+  cout << "li: ";
+  print(li.begin(), li.end());
+  list<int> l1, l2;
+  l1.push_back(23);
+  l1.push_back(58);
+  l1.push_back(72);
+  l1.sort(Greater());
+  l2.push_back(22);
+  l2.push_back(23);
+  l2.push_back(49);
+  l2.push_back(77);
+  l2.sort(Greater());
+  l1.merge(l2, Greater());
+  /*
+l2: 
+l1: 77 72 58 49 23 23 22  比较器倒序排列
+   */
+  cout << "l2: ";
+  print(l2.begin(), l2.end());
+  cout << "l1: ";
+  print(l1.begin(), l1.end());
   return 0;
 }
 
