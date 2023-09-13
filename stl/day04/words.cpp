344c9856cf732d8c156d04494121e69369ebda3b
taizilinger123 <837337164@qq.com>
2023-09-13 22:38:38 +0800

    20230913

--- stl/day04/words.cpp
+++ stl/day04/words.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 using namespace std;
-int main(void) {
-   char c;
-   while (cin >> c)
-   cout << c << endl;
+int main(int argc, char* argv[]) {
+   if (argc < 2) {
+      cerr << "用法:" << argv[0] << "<单词文件>"<<endl;
+	  return -1;
+   }
+   ifstream ifs (argv[1]);
+   if (! ifs) {
+      cout << "文件打开失败！"<< endl;
+	  return -1;
+   }
+   string word;
+   map<string, int> msi;
+   while (ifs >> word)
+    
    return 0;
 }
