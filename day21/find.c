43c8d82a8f9a476b6ef31c78b8deed7fbe95b665
taizilinger123 <837337164@qq.com>
2023-09-20 11:09:12 +0800

    20230920

--- day21/find.c
+++ day21/find.c
@@ -9,11 +9,28 @@ int line_find(int data[],size_t size,int key){
   } 
   return -1;
 }
+int half_find(int data[],size_t size,int key)
+{//二分查找，只能用于有序数组
+   int left = 0; int right = size-1;
+   while(left <= right){
+      int mid = (left+right)/2;
+	  if(key<data[mid])//小于去前半部分
+		right = mid-1;//前一半
+	  else if(key>data[mid])
+		left = mid+1;
+	  else return mid;
+   }
+   return -1;
+}
 int main(){
-   int data[] = {1,2,5,6,7,4,8,9,3};//无序数组
+   /*int data[] = {1,2,5,6,7,4,8,9,3};//无序数组
    size_t size = sizeof(data)/sizeof(data[0]);//数组的长度
    int key = 7;//查找7
-   int res = line_find(data, size, key);
+   int res = line_find(data, size, key);*/
+   int data[] = {1,2,3,4,5,6,7,8,9};//有序数组
+   size_t size = sizeof(data)/sizeof(data[0]);//数组的长度
+   int key = 7;//查找7
+   int res = half_find(data, size, key);
    if(res == -1){
        printf("没有找到\n");
    }else{
