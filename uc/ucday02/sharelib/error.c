b8220fea7b0fd4399908a15812b7e866c0a41f95
taizilinger123 <837337164@qq.com>
2023-09-24 16:11:18 +0800

    20230924

--- uc/ucday02/sharelib/error.c
+++ uc/ucday02/sharelib/error.c
@@ -16,6 +16,11 @@ int rand10(){
    if(ra==5) return -1;
    return ra;
 }
+int max(int x,int y,int* r){
+  if(x == y) return -1;
+  *r = (x>y)?x:y;//指针拿到了最大值
+  return 0;//返回0代表正确
+}
 int main(){
   print("hello");
   char* st = iserror("erro");
@@ -24,4 +29,7 @@ int main(){
   int r = rand10();
   if(r == -1)printf("出错了\n");
   else printf("%d\n",r);
+  int res = max(3,3,&r);
+  if(res==-1) printf("出错了\n");
+  else printf("max=%d\n",r);
 }
