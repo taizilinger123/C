e045748ec5c8c5a1582f630149f8bd3c0998b507
taizilinger123 <837337164@qq.com>
2023-11-06 17:09:00 +0800

    20231106

--- uc/ucday04/sbrk.c
+++ uc/ucday04/sbrk.c
@@ -3,10 +3,19 @@
 
 int main(){
    printf("pid=%d\n",getpid());
-   void* p1 = sbrk(4);//映射1个内存页
-   void* p2 = sbrk(4);//地址连续
-   void* p3 = sbrk(4);
-   void* p4 = sbrk(4);
+   int* p1 = sbrk(4);//映射1个内存页
+   int* p2 = sbrk(4);//地址连续
+   int* p3 = sbrk(4);
+   int* p4 = sbrk(4);
    printf("p1=%p,p2=%p,p3=%p,p4=%p\n",p1,p2,p3,p4);
+   *p1 = 10;*p2 = 20;*p3 = 30;*p4 = 40;
+   sbrk(-4);//释放内存 4字节(只释放地址没有解除映射)
+   sbrk(-8);//释放内存 8字节
+   void* cur = sbrk(0);//取当前的位置->04
+   printf("cur=%p\n",cur);//只有p1有效
+   printf("*p1=%d,*p2=%d,*p3=%d\n",*p1,*p2,*p3);
+   int* p5 = sbrk(4);
+   *p5 = 50;
+   printf("*p2=%d\n",*p2);
    while(1);
 }
