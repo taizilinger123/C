8e88e65d8b2e790485120be716ad91512b7f9c1f
taizilinger123 <837337164@qq.com>
2024-01-21 22:59:59 +0800

    20240121

--- uc/ucday05/open.c
+++ uc/ucday05/open.c
@@ -17,4 +17,14 @@ int main(){
 }
 //man 2 open 
 //man 2 read
+//man 2 write
+// ssize_t read(int fd, void *buf, size_t count)
 //void * 是万能指针可以转化为char *,string *
+//void *buf void *比数组大，是首地址 size_t代表buf的大小
+/*
+int read(int fd, void* buf, size_t size);  读是从文件里的读到内存里去，写是从内存里写到文件里去。
+参数：fd      代表  一个打开的文件描述符
+	  buf            一块内存的首地址
+	  size            buf的大小(sizeof)
+返回：>0      实际读到的字节数											  ==0   标识  读到了文件尾											  -1       读文件出错
+*/
