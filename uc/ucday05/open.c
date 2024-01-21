90666cc97757d75e3f3ff10eff2a24d459d47fa8
taizilinger123 <837337164@qq.com>
2024-01-21 22:29:44 +0800

    20240121

---
+++ uc/ucday05/open.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+   printf("O_RDONLY=%d,O_WRONLY=%d,O_RDWR=%d\n",
+		O_RDONLY,O_WRONLY,O_RDWR);
+   printf("O_CREAT=%d,O_APPEND=%d\n",O_CREAT,O_APPEND);
+   int fd=open("a.txt",O_RDWR|O_CREAT|O_TRUNC,0664);
+   //如果新建文件，需要指定权限
+   if(fd == -1)
+    perror("open"),exit(-1);
+   printf("文件创建/打开成功\n");
+   printf("fd=%d\n",fd);
+   close(fd);
+}
