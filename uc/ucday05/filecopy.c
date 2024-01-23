1b761d3153e38aca7577572dd92ed6db7b78b665
taizilinger123 <837337164@qq.com>
2024-01-23 22:04:14 +0800

    20240123

---
+++ uc/ucday05/filecopy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+  int fdr = open("a.txt",O_RDONLY);
+  if(fdr == -1) perror("open a"),exit(-1);
+  int fdw = open("b.txt",O_RDWR|O_CREAT|O_TRUNC,0666);
+  if(fdw == -1) perror("open b"),exit(-1);
+  char buf[3] = { };
+  while(1){
+     int res = read(fdr,buf,sizeof(buf));
+     if(res == -1) perror("read"),exit(-1);
+	 if(res == 0) break;
+     write(fdw,buf,res);
+  }
+  close(fdr);close(fdw);
+}
