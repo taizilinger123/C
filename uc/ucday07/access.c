e2c967d810e7394b2d79fe259b11385015c88bf0
taizilinger123 <837337164@qq.com>
2024-04-16 10:59:17 +0800

    20240416

--- uc/ucday07/access.c
+++ uc/ucday07/access.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 int main(){
    if(access("a.txt",F_OK)==0)
@@ -8,5 +9,8 @@ int main(){
    if(!access("a.txt",R_OK)) printf("可读\n");
    if(!access("a.txt",W_OK)) printf("可写\n");
    if(!access("a.txt",X_OK)) printf("可执行\n");
+
+   chmod("a.txt",0766);//改变文件权限
+   truncate("a.txt",100);//改变文件大小
 }
 
