4e65bb10104371655212eb04e6fbac4fa3d4a9a9
taizilinger123 <837337164@qq.com>
2024-04-16 14:55:56 +0800

    20240416

--- uc/ucday07/umask.c
+++ uc/ucday07/umask.c
@@ -4,10 +4,12 @@
 #include <sys/stat.h>
 
 int main(){
-	//系统的权限屏蔽字 002
+	mode_t old = umask(0123);//修改权限屏蔽字
+	//系统默认的权限屏蔽字 002
     int fd = open("b.txt",O_RDWR|O_CREAT,0777);
 	if(fd==-1) perror("open"),exit(-1);
 	struct stat s = {};
 	fstat(fd,&s);
 	printf("mode=%o\n",s.st_mode & 0777);
+	umask(old);//还原系统屏蔽字
 }
