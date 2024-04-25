ce3a3464e2850ea5be8fba4cadfbed6c842c3b5a
taizilinger123 <837337164@qq.com>
2024-04-25 13:32:59 +0800

    20240425

--- uc/ucday07/fork2.c
+++ uc/ucday07/fork2.c
@@ -6,9 +6,11 @@ int main(){
    printf("父进程开始运行\n");
    pid_t pid = fork();//创建子进程
    if(pid==0){//子进程
-     printf("我是子进程\n");
+     printf("我是子进程\n");//打印
+	 printf("子进程:%d,父进程:%d\n",getpid(),getppid());
    }else{//父进程
-     printf("我是父进程\n");
+     printf("我是父进程\n");//打印
+	 printf("子进程:%d,父进程:%d\n",pid(),getpid());
    }
    printf("进程%d over\n",getpid());
-}//练习：在子进程和父进程中，打印出父子进程的PID
+}//练习：分别在子进程和父进程中，打印出父子进程的PID
---
+++ uc/ucday07/fork3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int i1 = 100;
+int main(){
+   int i2 = 200;
+   char* st = malloc(20);
+   strcpy(st,"abcd");
+   pid_t pid = fork();
+   if(pid==-1) perror("fork"),exit(-1);
+   int i3 = 300;
+   if(pid==0){//子进程
+     i1=10; i2=20; i3=30; strcpy(st,"123");   
+	 printf("child:i1=%d,i2=%d,i3=%d,st=%s\n",i1,i2,i3,st);
+	 printf("child:&i1=%p,st=%p\n",&i1,st);
+	 exit(0);
+   }
+   sleep(1);//只有父进程执行
+   printf("father:i1=%d,i2=%d,i3=%d,st=%s\n",i1,i2,i3,st);
+   printf("father:&i1=%p,st=%p\n",&i1,st);
+
+}
