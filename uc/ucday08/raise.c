d57e84a5160b8945a27f48ae471c6f20f616e87a
taizilinger123 <837337164@qq.com>
2024-05-14 15:47:00 +0800

    20240514

--- uc/ucday08/raise.c
+++ uc/ucday08/raise.c
@@ -6,6 +6,22 @@ void fa(int signo){
 }
 int main(){
    signal(SIGINT,fa);
-   raise(2);
+   int res=sleep(10);//sleep()被非忽略信号中断
+   printf("res=%d\n",res);
+   raise(3);
    while(1);
 }
+
+/*
+ root@test:/home/test/biaoc/C/uc/ucday08# a.out
+ res=0    #这里让他自动跑完
+ 退出 (核心已转储)
+ root@test:/home/test/biaoc/C/uc/ucday08# a.out
+ ^C捕获了信号2  #这里本人按了ctrl+c
+ res=7          #信号在第7秒结束
+ 退出 (核心已转储)
+ root@test:/home/test/biaoc/C/uc/ucday08# a.out
+ ^C捕获了信号2 
+ res=8            
+ 退出 (核心已转储)
+ */
