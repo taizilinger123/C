9eba1dfc7963e29d41c6d8606d727915d8f1bca1
taizilinger123 <837337164@qq.com>
2024-05-22 16:31:51 +0800

    20240522

---
+++ uc/ucday09/itimer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
+
+void fa(int signo){
+  printf("i am superman\n");
+}
+int main(){
+  signal(SIGALRM,fa);
+  struct itimerval timer;
+  timer.it_interval.tv_sec=1;//间隔秒数
+  timer.it_interval.tv_usec = 100000;//微秒数
+  timer.it_value.tv_sec=3;//开始秒数
+  timer.it_value.tv_usec = 0;//微秒数
+  setitimer(ITIMER_REAL,&timer,0);
+  while(1);
+}
