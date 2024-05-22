fe334cf6356e1bfe3372fda873be02f9144b41dd
taizilinger123 <837337164@qq.com>
2024-05-22 10:15:01 +0800

    20240522

--- uc/ucday09/sigaction.c
+++ uc/ucday09/sigaction.c
@@ -4,12 +4,14 @@
 void fa(int signo){
   printf("捕获了信号%d\n",signo);
   sleep(5);
-}
+}//在信号处理函数执行期间，信号自身和sa_mask中的信号会被屏蔽
 int main(){
   struct sigaction action = {};
   action.sa_handler = fa;//指定信号处理函数
   sigemptyset(&action.sa_mask);
   sigaddset(&action.sa_mask,SIGQUIT);
+  //action.sa_flags = SA_RESETHAND;//只执行一次处理函数后恢复默认
+  action.sa_flags = SA_NOMASK;//在信号处理函数执行期间，解除对信号自身的屏蔽
   sigaction(SIGINT,&action,NULL);
   while(1);
 }
