552bf88b2b7ca79ef18634f8047402e9ffe27beb
taizilinger123 <837337164@qq.com>
2024-05-22 17:16:15 +0800

    20240522

--- uc/ucday09/itimer.c
+++ uc/ucday09/itimer.c
@@ -8,8 +8,10 @@ void fa(int signo){
 int main(){
   signal(SIGALRM,fa);
   struct itimerval timer;
+  //间隔时间1.1秒
   timer.it_interval.tv_sec=1;//间隔秒数
   timer.it_interval.tv_usec = 100000;//微秒数
+  //开始时间3秒(第一次)
   timer.it_value.tv_sec=3;//开始秒数
   timer.it_value.tv_usec = 0;//微秒数
   setitimer(ITIMER_REAL,&timer,0);
