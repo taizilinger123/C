e69f417b7ecb2cffd1f7fe9e9f167cbc95c577a1
taizilinger123 <837337164@qq.com>
2024-05-31 16:28:04 +0800

    20240531

--- uc/ucday10/shmb.c
+++ uc/ucday10/shmb.c
@@ -19,5 +19,34 @@ int main(){
  root@test:/home/test/biaoc/C/uc/ucday10# gcc shmb.c -ob
  root@test:/home/test/biaoc/C/uc/ucday10# b
  x=12345
+##########################################################
+root@test:/home/test/biaoc/C/uc/ucday10# ipcs -m 
+
+------------ 共享内存段 --------------
+键        shmid      拥有者  权限     字节     连接数  状态      
+0x00000000 65536      lightdm    600        524288     2          目标       
+0x00000000 163841     lightdm    600        524288     2          目标       
+0x00000000 229378     lightdm    600        67108864   2          目标       
+0x64011a25 294915     root       666        4          0                       
+
+root@test:/home/test/biaoc/C/uc/ucday10# ipcrm -m  65536
+root@test:/home/test/biaoc/C/uc/ucday10# ipcs -m 
+
+------------ 共享内存段 --------------
+键        shmid      拥有者  权限     字节     连接数  状态      
+0x00000000 65536      lightdm    600        524288     2          目标       
+0x00000000 163841     lightdm    600        524288     2          目标       
+0x00000000 229378     lightdm    600        67108864   2          目标       
+0x64011a25 294915     root       666        4          0                       
+
+root@test:/home/test/biaoc/C/uc/ucday10# ipcrm -m  294915
+root@test:/home/test/biaoc/C/uc/ucday10# ipcs -m 
+
+------------ 共享内存段 --------------
+键        shmid      拥有者  权限     字节     连接数  状态      
+0x00000000 65536      lightdm    600        524288     2          目标       
+0x00000000 163841     lightdm    600        524288     2          目标       
+0x00000000 229378     lightdm    600        67108864   2          目标
+当使用删除命令/函数  删除共享内存时，不确保立即删除。只是给共享内存做一个删除标记，当挂接进程数为0才能真正删除。
 
  */
