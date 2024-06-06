7b4161f0c4a05b171965246c8866c23e8e9424a0
taizilinger123 <837337164@qq.com>
2024-06-06 16:57:10 +0800

    20240606

---
+++ uc/ucday10/msgb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main(){
+  key_t key = ftok(".",100);
+  int msgid = msgget(key,0);
+  if(msgid==-1) perror("msgget"),exit(-1);
+  char buf[100] = {};
+  int res = msgrcv(msgid,buf,sizeof(buf),0,0);
+  if(res == -1) perror("msgrcv"),exit(-1);
+  printf("收到%d字节的数据:%s\n",res,buf);
+}
+
+/*
+ root@test:/home/test/biaoc/C/uc/ucday10# gcc msgb.c -ob
+ root@test:/home/test/biaoc/C/uc/ucday10# b
+ 收到9字节的数据:hello msg
+ root@test:/home/test/biaoc/C/uc/ucday10# b
+ 收到9字节的数据:hello msg
+ root@test:/home/test/biaoc/C/uc/ucday10# b #会阻塞
+#新开一个终端
+root@test:/home/test/biaoc/C/uc/ucday10# pwd
+/home/test/biaoc/C/uc/ucday10
+root@test:/home/test/biaoc/C/uc/ucday10# a
+发送成功
+ root@test:/home/test/biaoc/C/uc/ucday10# b
+ 收到9字节的数据:hello msg
+
+ */
