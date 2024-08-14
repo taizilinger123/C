037dadd8d1518a08d767d78cdd740b1f50a0b359
taizilinger123 <837337164@qq.com>
2024-08-14 15:33:08 +0800

    20240814

---
+++ uc/ucday13/tcpclient.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(){
+   int sockfd = socket(AF_INET,SOCK_STREAM,0);
+   if(sockfd == -1) perror("socket"),exit(-1);
+   struct sockaddr_in addr;//server端通信地址
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(2222);//本机转网络格式htons=host to network short
+   addr.sin_addr.s_addr = inet_addr("10.0.1.128");//转换IP的函数，点分十进制转换为十六进制
+   int res = connect(sockfd,(struct sockaddr*)&addr,sizeof(addr));
+   if(res == -1) perror("connect"),exit(-1);
+   printf("connect ok\n");
+   write(sockfd,"hello world",strlen("hello world"));
+   char buf[100] = {};
+   read(sockfd,buf,100);
+   printf("%s\n",buf);
+   close(sockfd);
+}//练习：socketd.c实现，发一个hello过来
---
+++ uc/ucday13/tcpserver1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(){
+  int sockfd = socket(AF_INET,SOCK_STREAM,0);
+  if(sockfd == -1) perror("socket"),exit(-1);
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(2222);
+  addr.sin_addr.s_addr = inet_addr("10.0.1.128");
+  int res = bind(sockfd,(struct sockaddr*)&addr,sizeof(addr));
+  if(res==-1) perror("bind"),exit(-1);
+  printf("bind ok\n");
+  listen(sockfd,100);//4 监听
+  struct sockaddr_in from;//用于存客户端地址
+  socklen_t len = sizeof(from);//传入传出参数
+  int fd = accept(sockfd,(struct sockaddr*)&from,&len);
+  printf("%s连接了服务器\n",inet_ntoa(from.sin_addr));//十六 转 点分十进制
+  char buf[100] = {};
+  read(fd,buf,100);
+  printf("%s\n",buf);
+  write(fd,"welcome",7);
+  close(fd); close(sockfd);
+}
